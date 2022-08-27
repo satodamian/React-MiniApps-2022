f3ffc523bc72700d1c7c2850ed0734876e1f33a4
SatoDQ <satowird93@gmail.com>
2022-08-26 21:22:30 -0500

    BreakingBAD 2022

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+
+import { useEffect, useState } from "react";
+import { Quote } from "./components/Quote";
+import Spinner from "./components/Spinner";
+const initialQuote = {
+    text: 'Quote',
+    author: 'Author',
+}
 
 function App() {
+
+  const [quote, setQuote] = useState(initialQuote);
+  const [loading, setLoading] = useState(false);
+
+  // UseEffect no recibe funciones asincornas
+  const updateQuotes= async()=>{
+    setLoading(true);
+    const url = "https://www.breakingbadapi.com/api/quote/random";
+    const res = await fetch(url);
+    const [newQuote] = await res.json();
+    const { quote: text, author } = newQuote;
+  
+    // nuevo estado del Quote
+    setQuote({
+      text: text,
+      author: author,
+    })
+      setLoading(false);
+  }
+
+  useEffect(() => {
+      updateQuotes();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <img
+        src="https://upload.wikimedia.org/wikipedia/commons/7/77/Breaking_Bad_logo.svg"
+        alt="logo"
+      />
+      <button onClick={()=>updateQuotes()}>Get Another</button>
+      {
+        loading ? <Spinner/>
+              : <Quote quote={quote}/>
+      }
+      
     </div>
   );
-}
+  }
 
 export default App;
---
+++ src/components/Quote.js
@@ -0,0 +1,14 @@
+
+
+const Quote = ({quote}) => {
+  return (
+    <p>
+        {quote.text} <br />
+        <span> -{quote.author}</span>
+      </p>
+  )
+}
+
+export {
+    Quote,
+}
